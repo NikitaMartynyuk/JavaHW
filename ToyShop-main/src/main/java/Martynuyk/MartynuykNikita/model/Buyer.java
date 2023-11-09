80a4ca7ca92b4813ede94148dfac726acac50383
NikitaMartynyuk <130921057+NikitaMartynyuk@users.noreply.github.com>
2023-11-09 15:31:48 +0500

    Add files via upload

---
+++ ToyShop-main/src/main/java/Martynuyk/MartynuykNikita/model/Buyer.java
@@ -0,0 +1,13 @@
+public record Buyer(int id, String name, String surname, int checkNumber, int phoneNumber) {
+
+
+    @Override
+    public String toString() {
+        return "\nПокупатель " +
+                "№ " + id +
+                ", '" + name + '\'' +
+                ", '" + surname + '\'' +
+                ", чек: " + checkNumber +
+                ", телефон: " + phoneNumber;
+    }
+}
